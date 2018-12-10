1f4f4944bac89fb48a1b7e4d65afca0524d9f346
srk1710 <38081877+srk1710@users.noreply.github.com>
2018-12-10 15:59:47 +0000

    Add files via upload

---
+++ TrabalhoPratico/src/Utilizadores/Utilizador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilizadores;
+
+/**
+ *
+ * @author srk
+ */
+public class Utilizador {
+    private String nome;
+    private int numcc;
+    private int nif;
+    private int contacto;
+    private String morada;
+    private String localidade;
+    
+    
+    
+    
+}
