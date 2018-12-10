8858bd7e9163a9566e44fd94bc6799dbac1295b8
phvsilva <39296984+phvsilva@users.noreply.github.com>
2018-12-10 16:42:29 +0000

    update_getters_setters_Conta

--- TrabalhoPratico/src/Utilizadores/Conta.java
+++ TrabalhoPratico/src/Utilizadores/Conta.java
@@ -29,5 +29,53 @@ public Conta(String nome, int numcc, int nif, int contacto, String morada, Strin
         this.localidade = localidade;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumcc() {
+        return numcc;
+    }
+
+    public void setNumcc(int numcc) {
+        this.numcc = numcc;
+    }
+
+    public int getNif() {
+        return nif;
+    }
+
+    public void setNif(int nif) {
+        this.nif = nif;
+    }
+
+    public int getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(int contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
 //METODOS DE INSTANCIA
 
