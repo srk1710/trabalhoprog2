350b80bb7586a171894542ae5051335bba4af9d3
phvsilva <39296984+phvsilva@users.noreply.github.com>
2018-12-10 16:21:17 +0000

    Update_conta

---
+++ TrabalhoPratico/src/Utilizadores/Conta.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilizadores;
+
+/**
+ *
+ * @author Pedro Silva
+ */
+public abstract class Conta extends Utilizador implements Cloneable {
+    
+    //Variavéis de Instancia
+    private String nome;
+    private int numcc;
+    private int nif;
+    private int contacto;
+    private String morada;
+    private String localidade;
+}
+
+public Conta(String nome, int numcc, int nif, int contacto, String morada, String localidade, String user, String passwd) {
+        super(user, passwd);
+        this.nome = nome;
+        this.numcc = numcc;
+        this.nif = nif;
+        this.contacto = contacto;
+        this.morada = morada;
+        this.localidade = localidade;
+    }
