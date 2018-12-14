9dedb126d8ac6dca66d8896e9b199e098026e930
Diogo <dicorreia@ipvc.pt>
2018-12-14 18:27:39 +0000

    criação respositorio

---
+++ TrabalhoPratico/src/MedHut/Repositorio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MedHut;
+import java.io.*;
+import java.util.*;
+import Utilizadores.*;
+
+/**
+ *
+ * @author srk
+ */
+public class Repositorio {
+    
+    // mano aqui sao declaradas aqueles maps que ele tem no enunciado
+    // mas mm assim acho que é preciso ter-mos um Set/list de cada classe
+    // para conseguires guardar um certo numero de classes q sao instanciadas
+    // temos de construir as classes que estão dentro de Med Hut primeiro
+    
+    private static Repositorio bd=null;
+    
+    
+    
+}
