a0bfade93fa9105cd637011127826e05ca9a25f2
Diogo <dicorreia@ipvc.pt>
2018-12-28 15:59:37 +0000

    update pacote medhut
    variaveis principais declaradas em todas as classes

--- TrabalhoPratico/src/MedHut/Consulta.java
+++ TrabalhoPratico/src/MedHut/Consulta.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package MedHut;
+
 import java.util.*;
 import Utilizadores.*;
 import java.io.Serializable;
@@ -12,72 +13,17 @@ import java.io.Serializable;
  *
  * @author srk
  */
-public class Consulta implements Serializable{
+public class Consulta extends Marcacao implements Serializable{
     
-    // indice de consulta
-    private static int numConsulta=0;
-    
-    // variaveis consulta
-    private int idCondulta;
-    private Date dataConsulta;
-    private Date inicioConsulta;
-    private Cliente cliente;
     private float duracaoConsulta;
     private float duracaoMaxima=1;    // corresponde à duracao maxima em horas
     private float preco;
-    private boolean confirmado;
+    private Date inicioConsulta;
     private boolean pago;
     private Set<ArtigoDespesa> despesas;
-    
-    public Consulta(Date dataConsulta, Date inicioConsulta, Cliente cliente, float duracaoConsulta, float preco){
-        idCondulta = Consulta.numConsulta +1;
-        this.dataConsulta=dataConsulta;
-        this.inicioConsulta=inicioConsulta;
-        this.cliente=cliente;
-        this.duracaoConsulta=duracaoConsulta;
-        this.preco=preco;
-        this.despesas= new HashSet<>();
-        Consulta.numConsulta++;
-    }
 
-    public static int getNumConsulta() {
-        return numConsulta;
-    }
-
-    public static void setNumConsulta(int numConsulta) {
-        Consulta.numConsulta = numConsulta;
-    }
-
-    public int getIdCondulta() {
-        return idCondulta;
-    }
-
-    public void setIdCondulta(int idCondulta) {
-        this.idCondulta = idCondulta;
-    }
-
-    public Date getDataConsulta() {
-        return dataConsulta;
-    }
-
-    public void setDataConsulta(Date dataConsulta) {
-        this.dataConsulta = dataConsulta;
-    }
-
-    public Date getInicioConsulta() {
-        return inicioConsulta;
-    }
-
-    public void setInicioConsulta(Date inicioConsulta) {
-        this.inicioConsulta = inicioConsulta;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
+    public Consulta(Date dataConsulta, Cliente cliente) {
+        super(dataConsulta, cliente);
     }
 
     public float getDuracaoConsulta() {
@@ -104,12 +50,12 @@ public class Consulta implements Serializable{
         this.preco = preco;
     }
 
-    public boolean isConfirmado() {
-        return confirmado;
+    public Date getInicioConsulta() {
+        return inicioConsulta;
     }
 
-    public void setConfirmado(boolean confirmado) {
-        this.confirmado = confirmado;
+    public void setInicioConsulta(Date inicioConsulta) {
+        this.inicioConsulta = inicioConsulta;
     }
 
     public boolean isPago() {
--- TrabalhoPratico/src/MedHut/Marcacao.java
+++ TrabalhoPratico/src/MedHut/Marcacao.java
@@ -5,10 +5,74 @@
  */
 package MedHut;
 
+import java.util.*;
+import Utilizadores.*;
+import java.io.Serializable;
+
 /**
  *
  * @author srk
  */
-public class Marcacao {
-    private 
+
+public class Marcacao implements Serializable{
+    
+    // indice de consulta
+    private static int numConsulta=0;
+    
+    // variaveis consulta
+    private int idConsulta;
+    private Date dataConsulta;
+    private Cliente cliente;
+    private boolean confirmado;
+    
+    
+    
+    public Marcacao(Date dataConsulta, Cliente cliente){
+        idConsulta = Marcacao.numConsulta +1;
+        this.dataConsulta = new Date();
+        this.cliente = cliente;
+        Marcacao.numConsulta++;
+    }
+
+    public static int getNumConsulta() {
+        return numConsulta;
+    }
+
+    public static void setNumConsulta(int numConsulta) {
+        Marcacao.numConsulta = numConsulta;
+    }
+
+    public int getIdConsulta() {
+        return idConsulta;
+    }
+
+    public void setIdConsulta(int idConsulta) {
+        this.idConsulta = idConsulta;
+    }
+
+    public Date getDataConsulta() {
+        return dataConsulta;
+    }
+
+    public void setDataConsulta(Date dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public boolean isConfirmado() {
+        return confirmado;
+    }
+
+    public void setConfirmado(boolean confirmado) {
+        this.confirmado = confirmado;
+    }
+    
+    
 }
