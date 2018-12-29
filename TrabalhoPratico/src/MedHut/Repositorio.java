4ebe4849bcda74ceb62070709c22f2e01d38dadd
Diogo <dicorreia@ipvc.pt>
2018-12-29 17:52:35 +0000

    update repositorio
    metodo serializar e desserializar

--- TrabalhoPratico/src/MedHut/Repositorio.java
+++ TrabalhoPratico/src/MedHut/Repositorio.java
@@ -20,12 +20,11 @@ public class Repositorio implements Serializable{
     
     private static Repositorio bd=null;
     
-    private Repositorio(){
+    Repositorio(){
         this.utilizadores = new ArrayList<>();
         this.ConsultorioEspecialidade = new HashMap<>();
         this.ConsultorioLocalidade = new HashMap<>();
     }
-    
     
     public static Repositorio getInstance(){
         if( bd == null){
@@ -34,5 +33,37 @@ public class Repositorio implements Serializable{
         
         return bd;
     }
+    
+    // getter e adicionar do list e dos maps
+    // da uma olhadela no repositorio do exemplo que está no moodle
+    
+    
+    
+    public static void serializar(String filename) {
+        // Serializar um objeto para ficheiro
+        try(FileOutputStream fileOut = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut))
+        {
+            out.writeObject(bd);
+            System.out.println("Serialized data is saved in " + filename);
+        } catch (IOException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+    }
+   
+    public static void desserializar(String filename) {
+        try {
+            FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            bd = (Repositorio) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Cliente class not found. " + ex.getMessage());
+        }
+    }
+
     
 }
